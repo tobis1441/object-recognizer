7f4a69abd995df8e66f59b7b68b6e6e7a9d949fe
tobi <tobias.switula@crowdcode.io>
2024-06-15 01:30:28 +0200

    Readme und paths anpassen

--- README.md
+++ README.md
@@ -17,3 +17,14 @@ conda install ...
 and export your dependencies into the environment.yml with:
 
 conda env export --no-builds -c conda-forge
+
+
+
+
+Now download the model from:
+
+https://github.com/OlafenwaMoses/ImageAI/releases/download/3.0.0-pretrained/retinanet_resnet50_fpn_coco-eeacb38b.pth/
+
+rename it to model_retinanet.pth and put it at root level of project.
+
+You now should be able to run main.py and get printed out the result from object recognition in the given image.
--- src/main/main.py
+++ src/main/main.py
@@ -3,9 +3,9 @@ print("Importing done..")
 
 detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
-detector.setModelPath("../../retinanet_resnet50_fpn_coco-eeacb38b.pth")
+detector.setModelPath("../../model_retinanet.pth")
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="../resources/image.jpeg", output_image_path="image2.jpg")
+detections = detector.detectObjectsFromImage(input_image="../resources/image.jpeg", output_image_path="../resources/output_image.jpg")
 
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
