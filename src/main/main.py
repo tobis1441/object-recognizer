df50a31e852845a0c9aa487e960ba1af8a046731
tobi <tobias.switula@crowdcode.io>
2024-06-15 12:24:47 +0200

    first test to check actions. Not expected to run, since dependencies are not in requirements.txt

--- src/main/main.py
+++ src/main/main.py
@@ -1,11 +1,22 @@
 from imageai.Detection import ObjectDetection
+
 print("Importing done..")
 
-detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
-detector.setModelPath("../../model_retinanet.pth")
-detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="../resources/image.jpeg", output_image_path="../resources/output_image.jpg")
 
-for detection in detections:
-    print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
+def run():
+    detector = ObjectDetection()
+    detector.setModelTypeAsRetinaNet()
+    detector.setModelPath("../../model_retinanet.pth")
+    detector.loadModel()
+
+    for i in range(1, 6):
+        print("Detecting objects in image ", i)
+        detections = detector.detectObjectsFromImage(input_image=f"../resources/image{i}.jpeg",
+                                                     output_image_path=f"../resources/output_image{i}.jpg")
+
+        for detection in detections:
+            print(detection["name"], " : ", detection["percentage_probability"])
+
+
+if __name__ == "__main__":
+    run()
---
+++ src/test/test_main.py
@@ -0,0 +1,4 @@
+from main.main import run
+
+def test_application_start():
+    run()
